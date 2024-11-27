8e08d9774847ca41d18138cbda0943e1f29ec102
Henry Hein <henry@regentmarkets.com>
2024-11-27 10:49:51 +0800

    fix: release error

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import packageJson from './package.json' assert { type: 'json' }
+import packageJson from './package.json' with { type: 'json' }
 import terser from '@rollup/plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
